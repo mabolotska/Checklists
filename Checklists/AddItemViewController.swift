7517e7e7c77ae9937b3ded1147a876e120694a5a
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-09-03 18:17:29 +0200

    added delegates

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -7,24 +7,72 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
-    
 
+protocol AddItemViewControllerDelegate: AnyObject {
+  func addItemViewControllerDidCancel(
+    _ controller: AddItemViewController)
+  func addItemViewController(
+    _ controller: AddItemViewController,
+    didFinishAdding item: ChecklistItem
+  )
+}
+
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    
+ weak var delegate: AddItemViewControllerDelegate?
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
     @IBOutlet weak var textField: UITextField!
     
     override func tableView(
-      _ tableView: UITableView,
-      willSelectRowAt indexPath: IndexPath
+        _ tableView: UITableView,
+        willSelectRowAt indexPath: IndexPath
     ) -> IndexPath? {
-    return nil
+        return nil
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
     
     @IBAction func cancel() {
-      navigationController?.popViewController(animated: true)
+      //  navigationController?.popViewController(animated: true)
+        
+        delegate?.addItemViewControllerDidCancel(self)
     }
     @IBAction func done() {
-      navigationController?.popViewController(animated: true)
+        
+        let item = ChecklistItem()
+         item.text = textField.text!
+         delegate?.addItemViewController(self, didFinishAdding: item)
+        
     }
+    
+    // MARK: - Text Field Delegates
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+    ) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(
+            in: stringRange,
+            with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
+    
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
+    
 }
