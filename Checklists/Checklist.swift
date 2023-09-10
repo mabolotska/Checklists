34f6c8a691122be563a20fd0128cdea7ed9eb0de
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-09-10 15:33:49 +0200

    detailVC

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,8 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by Maryna Bolotska on 09/09/23.
+//
+
+import Foundation
---
+++ Checklists/ListDetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ListDetailViewController.swift
+//  Checklists
+//
+//  Created by Maryna Bolotska on 10/09/23.
+//
+
+import Foundation
